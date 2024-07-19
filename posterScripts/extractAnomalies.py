151336156595b71b951ac80cf550b037dec9f3b8
iankahn7 <ikahn.2001@gmail.com>
2024-07-19 12:51:36 -0600

    anomalies to csv now working
    anomalies to csv now working

--- posterScripts/extractAnomalies.py
+++ posterScripts/extractAnomalies.py
@@ -1,19 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul 19 08:15:43 2024
-
-@author: ikahn
-"""
-
-# extract the anomalies 
-# generate heat map of correlation between sites of all the anomalies
-
-
-"""
-extract anomalies from monthly_average_water_levels.csv , generate heat
-map of correlation between all anomalies
-"""
-
 import pandas as pd
 
 # File paths
@@ -23,39 +7,33 @@ file_path2 = r'C:\Users\ikahn\Desktop\unm\IanKahn_RESUMES\ocean_sci\usf_making_w
 # Read the CSV files
 dfAverages = pd.read_csv(file_path1)
 dfLongTermAverages = pd.read_csv(file_path2)
-print(dfLongTermAverages)
 
-# Assuming the DataFrames are structured with the same columns for stations and months
+finalData = []
 
 # Initialize a DataFrame to store anomalies
 dfAnomalies = pd.DataFrame(index=dfAverages.index, columns=dfAverages.columns)
-for i,stationRow in dfAverages.iterrows():
+for i, stationRow in dfAverages.iterrows():
+    stationName = stationRow[0]
+    print(f"=== {stationName} ===")
+    anomalyRow = [stationName]
+    
+    for date in stationRow.index[1:]:
+        splitMonth = int(date.split("-")[1])
+        monthVal = stationRow[date]
+        climatologyVal = dfLongTermAverages.loc[dfLongTermAverages['month'] == splitMonth, stationName].values     
+        finalVal = monthVal - climatologyVal
+        anomalyRow.append(finalVal[0])
+        
+    print(anomalyRow)
+    finalData.append(anomalyRow)
 
-  stationName = stationRow[0]
-  print(f"=== {stationName} ===")
-  anomalyRow = [stationName]
-  
-  for date in stationRow.index[1:]:
-      #print(date)
-      splitMonth = int(date.split("-")[1])
-      #print(splitMonth)
-      climatologyVal = dfLongTermAverages.loc[dfLongTermAverages['month'] == splitMonth, stationName].values 
-      print(climatologyVal)
+# Create DataFrame from finalData
+df = pd.DataFrame(finalData)
 
-      
-      """
-      dateVal = stationRow[date]
-      print(dateVal)
-      splitMonth = dateVal.split()[1]
-      climatologyVal = dfLongTermAverages["month" == int(splitMonth)][stationName] 
-      print(climatologyVal) 
-      # split the string by the hyphen
-      # (2nd part is the month, index with a 1)
-      #filter for the month on the df longterm averages , get the coumn with station name 
-      # then take month avg value - longterm avg value
-      
-      anomalyRow.append(newVal)
-      """
+# Add header
+headers = ['Station'] + [date for date in dfAverages.columns[1:]]
+df.columns = headers
 
-
-
+# Save to CSV
+output_file_path = r'C:\Users\ikahn\Desktop\unm\IanKahn_RESUMES\ocean_sci\usf_making_waves\Research\ExploreMarineDataUsf\posterScripts\anomalies.csv'
+df.to_csv(output_file_path, index=False)
---
+++ posterScripts/makeHeatMapfromAnomalies.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 19 12:29:47 2024
+
+@author: ikahn
+"""
+
